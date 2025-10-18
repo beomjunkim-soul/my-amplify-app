a27b73f8ae9eef57078d8d05fd4ee6738e2901ac
beomjunkim-soul <kbg142337@naver.com>
2025-10-18 11:25:23 +0900

    feat: 수정

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -6,7 +6,7 @@ import axios from "axios";
 const api = axios.create({
   // baseURL: "http://localhost:8080", // 로컬 Spring API 서버 주소
   //baseURL: "http://54.180.24.104:8080", // my aws ec2 서버주소
-  baseURL: "https://taily98.store", //https로 요청, 기본포트 443, ALB <-> AWS EC2 Spring API 서버 주소
+  baseURL: "https://taily98.shop", //https로 요청, 기본포트 443, ALB <-> AWS EC2 Spring API 서버 주소
   headers: {
     "Content-Type": "application/json",
   },
